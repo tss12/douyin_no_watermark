1d07223848d707144bae140c8c5b0e06410ae903
tss12 <tansongsong00@163.com>
2019-04-14 18:38:26 +0800

    Create no_waterMark_video.py
    无水印抖音视频，原理很简单。

---
+++ no_waterMark_video.py
@@ -0,0 +1,63 @@
+import requests
+import configparser
+import os
+
+
+from requests.packages import urllib3
+urllib3.disable_warnings()
+
+def getHeaders( filename, key ):
+    conf = configparser.ConfigParser()
+    conf.read( filename );
+    confDict = dict(conf._sections);
+    headers = dict(confDict[key]);
+    return headers;
+
+def parseUrl( url, headers ):    
+    res = requests.get( url, headers=headers, verify=False );    
+    res.encoding = 'utf-8'
+    # print(res.text)
+    # return
+    data = res.text
+    cover = data.split("cover: \"")[1].split("\"")[0]
+    playAddr = data.split("playAddr: \"")[1].split("\",")[0]
+    videoId = playAddr.split("video_id=")[1].split("&")[0]
+    # https://www.iesdouyin.com/share/video/6679599825291316494/?region=CN&mid=6679412062147332872&u_code=l1b63kge&titleType=title&timestamp=1555231715&utm_campaign=client_share&app=aweme&utm_medium=ios&tt_from=copy&utm_source=copy&iid=59857117820
+    videoAddr = playAddr.replace("/playwm/","/play/");
+    print (videoAddr)
+    return
+    return {
+        "cover": cover,
+        "playAddr": playAddr,
+        "addr": videoAddr,
+        "id": videoId
+    }
+
+def downVideo( parseDouyin, headers ):
+   
+    videoBin = requests.get( parseDouyin['addr'], headers=headers, verify=False );
+    _filename = parseDouyin['id'] + ".mp4";
+    with open( _filename, "wb") as f:
+            f.write(videoBin.content)
+            f.close()
+    return _filename;
+
+
+
+
+#主函数
+if __name__ == '__main__':
+    print("点击转发抖音视频时，可通过选择复制链接来获取视频链接\n")
+    url = input("请输入抖音视频url：");
+
+    headers = getHeaders( "config.ini", "headers" );
+    arduinoHeaders = getHeaders( "config.ini", "arduino-headers" );
+
+    parseUrl( url, headers);
+    # parseData = parseUrl( url, headers);
+    # print( "视频源地址为：" + parseData['addr'] );
+    # 下载视频
+    # filename = downVideo( parseData, arduinoHeaders );
+    # print( "视频下载完成，", "视频Id为：",parseData['id'], "\n" );
+    print( "视频保存在当前同级目录下，格式为mp4\n" );
+    
